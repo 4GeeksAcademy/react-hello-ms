2d07e23b2ddea5880d06a6df523d283c0a51638a
matii17 <154836261+matii17@users.noreply.github.com>
2024-05-21 18:49:37 +0000

    Mi mensaje

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,59 +2,6 @@ import React from 'react';
 
 const Card = ({ }) => {
   return (
-    /*<div className='container'>
-    <div className='row p-4 row-cols-1 row-cols-md-4 g-4'>
-      <div className='col-lg'>
-    <div className="card text-center mb-4" style={{ width: '18rem' }} >
-  <img src="https://picsum.photos/id/28/367/267" className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Go somewhere</a>
-  </div>
-</div>
-</div>
-<div className='col-lg'>
-    <div className="card text-center mb-4" style={{ width: '18rem' }} >
-  <img src="https://picsum.photos/id/28/367/267" className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Go somewhere</a>
-  </div>
-</div>
-</div>
-<div className='col-lg'>
-    <div className="card text-center mb-4" style={{ width: '18rem' }} >
-  <img src="https://picsum.photos/id/28/367/267" className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    
-  </div>
-  <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Go somewhere</a>
-  </div>
-</div>
-</div>
-<div className='col-lg'>
-    <div className="card text-center mb-4" style={{ width: '18rem' }} >
-  <img src="https://picsum.photos/id/28/367/267" className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Go somewhere</a>
-  </div>
-</div>
-</div>
-</div>
-</div>*/
 
 <div className='container'>
 <div className="row row-cols-1 row-cols-md-4 g-4">
@@ -65,7 +12,7 @@ const Card = ({ }) => {
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
      <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Go somewhere</a>
+  <a href="#" className="btn btn-primary ">Find Out More!</a>
   </div>
         </div>
     </div>
@@ -77,7 +24,7 @@ const Card = ({ }) => {
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Go somewhere</a>
+  <a href="#" className="btn btn-primary ">Find Out More!</a>
   </div>
         </div>
     </div>
@@ -89,7 +36,7 @@ const Card = ({ }) => {
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer..</p>
      <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Go somewhere</a>
+  <a href="#" className="btn btn-primary ">Find Out More!</a>
   </div>
         </div>
     </div>
@@ -101,7 +48,7 @@ const Card = ({ }) => {
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Go somewhere</a>
+  <a href="#" className="btn btn-primary ">Find Out More!</a>
   </div>
         </div>
     </div>
