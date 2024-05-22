8324dabea1efd1c1576e7137fcffbb5070085867
matii17 <154836261+matii17@users.noreply.github.com>
2024-05-22 05:31:44 +0000

    comentario

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,58 +2,6 @@ import React from 'react';
 
 const Card = ({title, description, buttonText, imageUrl }) => {
   return (
-/*<div className='container'>
-<div className="row row-cols-1 row-cols-md-4 g-4">
-  <div className="col">
-    <div className="card text-center ">
-      <img src="https://picsum.photos/id/28/367/267" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-     <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Find Out More!</a>
-  </div>
-        </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card text-center">
-      <img src="https://picsum.photos/id/29/367/267" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Find Out More!</a>
-  </div>
-        </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card text-center">
-      <img src="https://picsum.photos/id/37/367/267" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer..</p>
-     <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Find Out More!</a>
-  </div>
-        </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card text-center">
-      <img src="https://picsum.photos/id/35/367/267" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <div className='card-footer'>
-  <a href="#" className="btn btn-primary ">Find Out More!</a>
-  </div>
-        </div>
-    </div>
-  </div>
-</div>
-</div>*/
 
     <div className="col">
       <div className="card text-center" >
